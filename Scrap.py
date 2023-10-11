426bcfa1402a8500215f4ad6e11bc41f85907578
Precious Edmund <Chidera1915@gmail.com>
2023-10-11 13:54:51 +0000

    Push

--- Scrap.py
+++ Scrap.py
@@ -6,10 +6,15 @@ url = 'https://wallpapercave.com/aggretsuko-wallpapers'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the data you want to scrape from the HTML
-title = soup.find('title').text
-description = soup.find('meta', property='description')['content']
+# Extract the image URLs from the HTML
+image_urls = []
+for image in soup.find_all('img', class_='wallpaper-image'):
+    image_urls.append(image['src'])
 
-# Print the scraped data
-print(title)
-print(description)
+# Save the image URLs to a file
+with open('aggretsuko_wallpapers.txt', 'w') as f:
+    for image_url in image_urls:
+        f.write(image_url + '\n')
+
+# Print the number of image URLs that were scraped
+print('The number of image URLs that were scraped is:', len(image_urls))
